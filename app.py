134b7babbfaf5d0b03baa8ea6724d11b3cc04c02
vivekgr92 <vivekgr92@gmail.com>
2024-01-20 11:15:03 -0800

    Fast api interface - upload and process image

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
+import uvicorn
+from fastapi import FastAPI, File, UploadFile, Form
+from fastapi.responses import HTMLResponse, JSONResponse, StreamingResponse
+from starlette.requests import Request
 from io import BytesIO
 import requests
-import gradio as gr
-import requests
 import torch
-from tqdm import tqdm
 from PIL import Image, ImageOps
 from diffusers import StableDiffusionInpaintPipeline
 from torchvision.transforms import ToPILImage
-from utils import preprocess, prepare_mask_and_masked_image, recover_image, resize_and_crop
+from utils import preprocess, prepare_mask_and_masked_image, recover_image, resize_and_crop, read_firebase_db
+import base64
+import pdb
+import numpy as np
 
-gr.close_all()
+
+app = FastAPI()
+
 topil = ToPILImage()
 
 pipe_inpaint = StableDiffusionInpaintPipeline.from_pretrained(
@@ -20,10 +26,10 @@ pipe_inpaint = StableDiffusionInpaintPipeline.from_pretrained(
 )
 pipe_inpaint = pipe_inpaint.to("cuda")
 
-## Good params for editing that we used all over the paper --> decent quality and speed   
-GUIDANCE_SCALE = 7.5
-NUM_INFERENCE_STEPS = 100
-DEFAULT_SEED = 1234
+## Good params for editing that we used all over the paper --> decent quality and speed 
+guidance_scale = 7.5
+num_inference_steps = 100
+seed = 1234
 
 def pgd(X, targets, model, criterion, eps=0.1, step_size=0.015, iters=40, clamp_min=0, clamp_max=1, mask=None):
     X_adv = X.clone().detach() + (torch.rand(*X.shape)*2*eps-eps).cuda()
@@ -54,6 +60,7 @@ def get_target():
     target_image = target_image.resize((512, 512))
     return target_image
 
+
 def immunize_fn(init_image, mask_image):
     with torch.autocast('cuda'):
         mask, X = prepare_mask_and_masked_image(init_image, mask_image)
@@ -80,104 +87,172 @@ def immunize_fn(init_image, mask_image):
         adv_image = recover_image(adv_image, init_image, mask_image, background=True)
         return adv_image        
 
-def run(image, prompt, seed, guidance_scale, num_inference_steps, immunize=False):
-    if seed == '':
-        seed = DEFAULT_SEED
-    else:
-        seed = int(seed)
+
+
+# @app.post("/upload")
+# async def upload_file(request: Request, file: UploadFile = File(...)):
+#     try:
+#         # Read the uploaded file as bytes
+#         contents = await file.read()
+
+#         # Process the image using PIL (you can replace this with your own processing logic)
+#         image = Image.open(BytesIO(contents))
+#         image_details = {
+#             "filename": file.filename,
+#             "content_type": file.content_type,
+#             "width": image.width,
+#             "height": image.height,
+#         }
+
+#         return JSONResponse(content=image_details, status_code=200)
+
+#     except Exception as e:
+#         return JSONResponse(content={"error": str(e)}, status_code=500)
+
+# @app.post("/upload")
+# async def upload_file(request: Request, file: UploadFile = File(...)):
+#     try:
+#         # Check if the request contains form data
+#         form_data = await request.form()
+
+#         # Read the uploaded file as bytes
+#         contents = await file.read()
+
+#         # Process the image using PIL (you can replace this with your own processing logic)
+#         image = Image.open(BytesIO(contents))
+
+#         prompt = "a man standing in a office"
+
+#         run(image, prompt, seed, guidance_scale, num_inference_steps, immunize=False)
+
+#         return {"message": "Successful"}
+    
+#     except Exception as e:
+#         return JSONResponse(content={"error": str(e)}, status_code=500)
+
+
+def generate_mask(image_size, initial_value=0):
+    # Create a NumPy array for the mask
+    mask = np.full(image_size, initial_value, dtype=np.uint8)
+    return mask
+
+# Function to convert image to base64
+def image_to_base64(image):
+    buffered = BytesIO()
+    image.save(buffered, format="JPEG")
+    return base64.b64encode(buffered.getvalue()).decode("utf-8")
+
+# Function to convert PIL image to bytes
+def image_to_bytes(image):
+    with BytesIO() as byte_io:
+        image.save(byte_io, format="JPEG")
+        return byte_io.getvalue()
+
+def process_image(image, prompt, seed, guidance_scale, num_inference_steps, immunize=False):
+
     torch.manual_seed(seed)
 
-    init_image = Image.fromarray(image['image'])
+    
+    init_image =  Image.fromarray(np.array(image))
     init_image = resize_and_crop(init_image, (512,512))
-    mask_image = ImageOps.invert(Image.fromarray(image['mask']).convert('RGB'))
-    mask_image = resize_and_crop(mask_image, init_image.size)
+   
+    # mask_image = ImageOps.invert(Image.fromarray(image['mask']).convert('RGB'))
+    # mask_image = resize_and_crop(mask_image, init_image.size)
+    mask_image = generate_mask(init_image .size)
+  
     
     if immunize:
         immunized_image = immunize_fn(init_image, mask_image)
         
     image_edited = pipe_inpaint(prompt=prompt, 
-                         image=init_image if not immunize else immunized_image, 
-                         mask_image=mask_image, 
-                         height = init_image.size[0],
-                         width = init_image.size[1],
-                         eta=1,
-                         guidance_scale=guidance_scale,
-                         num_inference_steps=num_inference_steps,
+                        image=init_image if not immunize else immunized_image, 
+                        mask_image=mask_image, 
+                        height = init_image.size[0],
+                        width = init_image.size[1],
+                        eta=1,
+                        guidance_scale=guidance_scale,
+                        num_inference_steps=num_inference_steps,
                         ).images[0]
         
     image_edited = recover_image(image_edited, init_image, mask_image)
     
+    # if immunize:
+    #     # Return the immunized image
+    #     return {"immunized_image": image_to_base64(immunized_image)}
+    # else:
+    #     return {"edited_image": image_to_base64(image_edited)}
+    
     if immunize:
-        return [(immunized_image, 'Immunized Image'), (image_edited, 'Edited After Immunization')]
+        # Return the immunized image
+        return image_to_bytes(immunized_image)
     else:
-        return [(image_edited, 'Edited Image (Without Immunization)')]
+        return image_to_bytes(image_edited)
+    
+    
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.post("/upload")
+async def upload_file(request: Request, payload: dict):
+    try:
+        # Extract image URL from the payload
+        image_url = payload.get("img")
+
+        # Download the image from the URL
+        response = requests.get(image_url)
+        response.raise_for_status()  # Check if the request was successful
+
+        # Process the image using PIL (you can replace this with your own processing logic)
+        image = Image.open(BytesIO(response.content))
+
+        prompt = "a man standing in an office"
+
+        # Assuming you have defined seed, guidance_scale, and num_inference_steps elsewhere
+        seed = 1234
+        guidance_scale = 7.5
+        num_inference_steps = 100
+
+        # Assuming you have defined immunize elsewhere
+        immunize = False
+
+        # Process image and store base64 in variable
+        global processed_image_base64
+
+        processed_image_base64 = process_image(image, prompt, seed, guidance_scale, num_inference_steps, immunize)
+
+        return {"message": "Image processed successfully."}
+
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
+
+# Route to get the processed image
+@app.get("/get_processed_image")
+async def get_processed_image():
+    try:
+        global processed_image_base64
+
+        if processed_image_base64:
+            # # Decode base64 to image bytes
+            # image_bytes = base64.b64decode(processed_image_base64)
+
+            # # Clear the variable after returning the image
+            # processed_image_base64 = None
+
+            # # Create a StreamingResponse to return the image
+            # return StreamingResponse(io.BytesIO(image_bytes), media_type="image/jpeg")
+            return StreamingResponse(BytesIO(processed_image_base64), media_type="image/jpeg")
+            # return processed_image_base64
+        else:
+            return JSONResponse(content={"message": "No processed image available."}, status_code=404)
+
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
 
 
-description='''
-Prevent malicious actors from using your photo to create DeepFakes and spread false information. Protect yourself today!
-<br />
-'''
+if __name__ == "__main__":
+    
 
-examples_list = [
-                    ['./images/hadi_and_trevor.jpg', 'man attending a wedding', '329357', GUIDANCE_SCALE, NUM_INFERENCE_STEPS],
-                    ['./images/trevor_2.jpg', 'two men in prison', '329357', GUIDANCE_SCALE, NUM_INFERENCE_STEPS],
-                    ['./images/elon_2.jpg', 'man in a metro station', '214213', GUIDANCE_SCALE, NUM_INFERENCE_STEPS],
-                ]
+    uvicorn.run(app, host="0.0.0.0", port=7860)
 
-
-with gr.Blocks() as demo:
-    gr.HTML(value="""<h1 style="font-weight: 900; margin-bottom: 7px; margin-top: 5px;">
-            DigiShield: Protect your photos online today from DeepFake technologies </h1><br>
-        """)
-    gr.Markdown(description)
-    with gr.Accordion(label='How to use (step by step):', open=False):
-        gr.Markdown('''
-            *First, let's edit your image:*        
-            + Upload an image (or select from the examples below)
-            + Use the brush to mask the parts of the image you want to keep unedited (e.g., faces of people)
-            + Add a prompt to guide the edit (see examples below)
-            + Play with the seed and click submit until you get a realistic edit that you are happy with (we provided good example seeds for you below)
-
-            *Now, let's immunize your image and try again:*
-            + Click on the "Immunize" button, then submit.
-            + You will get an immunized version of the image (which should look essentially identical to the original one) as well as its edited version (which should now look rather unrealistic)
-        ''')
-
-    # with gr.Accordion(label='Example (video):', open=False):
-    #     gr.HTML('''
-    #         <center>
-    #         <iframe width="920" height="600" src="https://www.youtube.com/embed/aTC59Q6ZDNM">
-    #         allow="fullscreen;" frameborder="0">
-    #         </iframe>
-    #         </center>
-    #     '''
-    #     )
-
-    with gr.Row():  
-        with gr.Column():
-            imgmask = gr.ImageMask(label='Drawing tool to mask regions you want to keep, e.g. faces')
-            prompt = gr.Textbox(label='Prompt', placeholder='A photo of a man in a wedding')
-            seed = gr.Textbox(label='Seed (Change to get different edits)', placeholder=str(DEFAULT_SEED), visible=True)
-            with gr.Accordion("Advanced Options", open=False):
-                scale = gr.Slider(label="Guidance Scale", minimum=0.1, maximum=25.0, value=GUIDANCE_SCALE, step=0.1)
-                num_steps = gr.Slider(label="Number of Inference Steps", minimum=10, maximum=250, value=NUM_INFERENCE_STEPS, step=5)
-            immunize = gr.Checkbox(label='Immunize', value=False)
-            b1 = gr.Button('Submit')
-        with gr.Column():
-            genimages = gr.Gallery(label="Generated images", 
-                       show_label=False, 
-                       elem_id="gallery").style(grid=[1,2], height="auto")
-            duplicate = gr.HTML("""
-                <p>For faster inference without waiting in queue, you may duplicate the space and upgrade to GPU in settings.
-                <br/>
-                <a href="https://huggingface.co/spaces/hadisalman/photoguard?duplicate=true">
-                <img style="margin-top: 0em; margin-bottom: 0em" src="https://bit.ly/3gLdBN6" alt="Duplicate Space"></a>
-                <p/>
-            """)
-            
-    b1.click(run, [imgmask, prompt, seed, scale, num_steps, immunize], [genimages])
-    examples = gr.Examples(examples=examples_list,inputs = [imgmask, prompt, seed, scale, num_steps, immunize],  outputs=[genimages], cache_examples=False, fn=run)
-
-
-# demo.launch()
-demo.launch(server_name='0.0.0.0', share=True, server_port=7860, inline=False)
